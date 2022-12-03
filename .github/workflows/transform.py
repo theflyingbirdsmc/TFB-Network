6251eb76ba917032020c053c50f10d5444d099c1
Simon Fugl <sifug20@student.sdu.dk>
2022-12-04 00:37:15 +0100

    Min næse løber lol

--- .github/workflows/transform.py
+++ .github/workflows/transform.py
@@ -4,9 +4,8 @@ import yaml
 file_path = os.environ['FILE_PATH']
 
 with open('/home/runner/work/TFB-Network/TFB-Network/.github/workflows/scripts/live_values.yml') as f:
-    live_values = yaml.load_all(f,Loader=yaml.FullLoader)
+    live_values = yaml.load_all(f, Loader=yaml.FullLoader)
 
 print("live values: ", live_values)
-for doc in live_values:
-    for k, v in doc.items():
-        print(k, "->", v)
+for k, v in live_values:
+    print(k, "->", v)
