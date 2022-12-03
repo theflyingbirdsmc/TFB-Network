98af205da31f8a5028346c3ce63252971b0d840d
Simon Fugl <sifug20@student.sdu.dk>
2022-12-03 23:48:29 +0100

    Loading live_values

--- .github/workflows/transform.py
+++ .github/workflows/transform.py
@@ -1,5 +1,14 @@
 import os
+import ruamel.yaml
 
 file_path = os.environ['FILE_PATH']
+yaml = ruamel.yaml.YAML()
+yaml.preserve_quotes = True
+
+with open('./scripts/live_values.yml') as fp:
+    live_values = yaml.load(fp)
+
+for k,v in live_values:
+  print("k:" + k + " v: " + v)
 
 print("File path:", file_path)
