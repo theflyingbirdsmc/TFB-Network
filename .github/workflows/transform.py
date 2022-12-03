6cc4f53066da882a3fba03282236502ec78f05e7
Simon Fugl <sifug20@student.sdu.dk>
2022-12-03 23:56:36 +0100

    Test

--- .github/workflows/transform.py
+++ .github/workflows/transform.py
@@ -5,10 +5,12 @@ file_path = os.environ['FILE_PATH']
 yaml = ruamel.yaml.YAML()
 yaml.preserve_quotes = True
 
-with open('/home/runner/work/TFB-Network/TFB-Network/.github/workflows/scripts/live_values.yml') as fp:
+with open('./scripts/live_values.yml') as fp:
     live_values = yaml.load(fp)
 
-for k,v in live_values:
-  print("k:" + k + " v: " + v)
+for element in live_values:
+  if file_path in element:
+    print(element)
+
 
 print("File path:", file_path)
