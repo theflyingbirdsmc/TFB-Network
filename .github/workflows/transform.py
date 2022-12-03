f01906ecdc115c1c261c777d55c6900d08780ace
Simon Fugl <sifug20@student.sdu.dk>
2022-12-04 00:22:38 +0100

    wtf

--- .github/workflows/transform.py
+++ .github/workflows/transform.py
@@ -7,8 +7,8 @@ yaml.preserve_quotes = True
 
 with open('/home/runner/work/TFB-Network/TFB-Network/.github/workflows/scripts/live_values.yml') as fp:
     live_values = yaml.load(fp)
-print("live values: " + live_values)
+print("live values: ", live_values)
 for element in live_values:
   if file_path in element:
-    print("1: " + element[1]) 
-    print("2: " + element[1][0])
+    print("1: ", element[1]) 
+    print("2: ", element[1][0])
