d556cdfe7bddb1dd272229301a66a42aea75c5de
Simon Fugl <sifug20@student.sdu.dk>
2022-12-03 23:52:48 +0100

    No such file

--- .github/workflows/transform.py
+++ .github/workflows/transform.py
@@ -5,7 +5,7 @@ file_path = os.environ['FILE_PATH']
 yaml = ruamel.yaml.YAML()
 yaml.preserve_quotes = True
 
-with open('./scripts/live_values.yml') as fp:
+with open('/home/runner/work/TFB-Network/TFB-Network/.github/workflows/scripts/live_values.yml') as fp:
     live_values = yaml.load(fp)
 
 for k,v in live_values:
