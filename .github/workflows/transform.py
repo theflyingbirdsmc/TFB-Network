9af270becea7904ff5fc5629e8d20fba8a71df62
Simon Fugl <sifug20@student.sdu.dk>
2022-12-04 11:28:43 +0100

    hope this works

--- .github/workflows/transform.py
+++ .github/workflows/transform.py
@@ -1,16 +1,17 @@
 import os
 import yaml
 
-file_path = os.environ['FILE_PATHS']
+file_path = os.environ['FILE_PATHS'].split(" ")
 print(file_path)
 
 with open('/home/runner/work/TFB-Network/TFB-Network/.github/workflows/scripts/live_values.yml') as f:
     live_values = yaml.load(f, Loader=yaml.FullLoader)
 
 print("live values: ", live_values)
-if file_path in live_values:
-    with open('/home/runner/work/TFB-Network/TFB-Network/' + file_path) as f:
-        yamlFileFromLive = yaml.load(f, Loader=yaml.FullLoader)
-    for value in live_values[file_path]:
-        
-        print(live_values[file_path][0])
+
+for file in file_path:
+    if file in live_values:
+        with open('/home/runner/work/TFB-Network/TFB-Network/' + file) as f:
+            yamlFileFromLive = yaml.load(f, Loader=yaml.FullLoader)
+        print(live_values[file_path])
+        # for value in live_values[file_path]
