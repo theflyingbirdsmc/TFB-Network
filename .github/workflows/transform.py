37622bb2625df9e3ec659d27122d79d31da3a495
Simon Fugl <sifug20@student.sdu.dk>
2022-12-04 11:00:48 +0100

    We only want this script to run once

--- .github/workflows/transform.py
+++ .github/workflows/transform.py
@@ -2,10 +2,15 @@ import os
 import yaml
 
 file_path = os.environ['FILE_PATH']
+print(file_path)
 
 with open('/home/runner/work/TFB-Network/TFB-Network/.github/workflows/scripts/live_values.yml') as f:
     live_values = yaml.load(f, Loader=yaml.FullLoader)
 
 print("live values: ", live_values)
 if file_path in live_values:
+    with open('/home/runner/work/TFB-Network/TFB-Network/' + file_path) as f:
+        yamlFileFromLive = yaml.load(f, Loader=yaml.FullLoader)
+    for value in live_values[file_path]:
+        
     print(live_values[file_path][0])
