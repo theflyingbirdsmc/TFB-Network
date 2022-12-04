911285693dfae302ea1fbffbff89c313466a02db
Simon Fugl <sifug20@student.sdu.dk>
2022-12-04 23:39:05 +0100

    We need to change the structure of live values

--- .github/workflows/transform.py
+++ .github/workflows/transform.py
@@ -1,6 +1,9 @@
 import os
 import ruamel.yaml
 
+yaml = ruamel.yaml.YAML()
+yaml.preserve_quotes = True
+
 live_values = os.environ.get('LIVE_VALUES')
 print(type(live_values))
 print(live_values)
@@ -12,11 +15,11 @@ print(live_values)
 # output = {}
 # name = 'getvalues_result'
 
-# for file in live_values:
-#     if file in live_values:
-#         with open('/home/runner/work/TFB-Network/TFB-Network/' + file, 'r') as f:
-#             yamlFileFromLive = yaml.unsafe_load(f)
-#         output[file] = yamlFileFromLive[live_values[file]]
+# for key, value in live_values:
+#     if key.startswith("TFB-"):
+#         with open('/home/runner/work/TFB-Network/TFB-Network/' + key, 'r') as f:
+#             yamlFileFromDev = yaml.load(f)
+#         if 
 
 # with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
 #     print(f'{name}={output}', file=fh)
