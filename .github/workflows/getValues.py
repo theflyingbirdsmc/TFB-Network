1448583a9c32c5425492909e9c2fba4e9e2a5519
Simon Fugl <sifug20@student.sdu.dk>
2022-12-04 21:59:17 +0100

    Without json worked fine

--- .github/workflows/getValues.py
+++ .github/workflows/getValues.py
@@ -1,6 +1,5 @@
 import os
 import yaml
-import json
 
 file_path = os.environ['FILE_PATHS'].split(" ")
 print(file_path)
@@ -18,27 +17,5 @@ for file in file_path:
             yamlFileFromLive = yaml.unsafe_load(f)
         output[file] = yamlFileFromLive[live_values[file]]
 
-json_object = {
-   "TFB-Creative/plugins/Plan/config.yml":{
-      "Type":"SQLite",
-      "MySQL":{
-         "Host":"localhost",
-         "Port":3306,
-         "User":"root",
-         "Password":"minecraft",
-         "Database":"Plan",
-         "Launch_options":"?rewriteBatchedStatements=true&useSSL=false&serverTimezone=UTC",
-         "Max_connections":8
-      }
-   },
-   "TFB-Flamecord/config.yml":{
-      "crowdcontrol":{
-         "address":"3333333333.18.0.1:25606",
-         "motd":"",
-         "restricted": false
-      }
-   }
-}
-
 with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-    print(f'{name}={json_object}', file=fh)
+    print(f'{name}={output}', file=fh)
