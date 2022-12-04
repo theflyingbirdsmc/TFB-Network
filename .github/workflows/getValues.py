1efae164945c3c77a008a8d2b0fdde3e6cfcf4de
Simon Fugl <sifug20@student.sdu.dk>
2022-12-04 13:58:19 +0100

    I must be Einstein

--- .github/workflows/getValues.py
+++ .github/workflows/getValues.py
@@ -15,4 +15,4 @@ for file in file_path:
         with open('/home/runner/work/TFB-Network/TFB-Network/' + file, 'r') as f:
             yamlFileFromLive = yaml.unsafe_load(f)
         output[file] = yamlFileFromLive[live_values[file]]
-print("f{getvalues_result}={output} >> $GITHUB_OUTPUT")
+print(output)
