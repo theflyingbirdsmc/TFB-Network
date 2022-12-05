f7a7f97da6dd883cdde92dba7c2c0944546c6e51
Simon Fugl <sifug20@student.sdu.dk>
2022-12-05 21:15:06 +0100

    Switch to ruamel.yaml the superior

--- .github/workflows/getValues.py
+++ .github/workflows/getValues.py
@@ -1,12 +1,12 @@
 import os
-import yaml
+import ruamel.yaml as yaml
 import json
 
 file_path = os.environ['FILE_PATHS'].split(" ")
 print(file_path)
 
-with open('/home/runner/work/TFB-Network/TFB-Network/.github/workflows/scripts/live_values.yml') as f:
-    live_values = yaml.load(f, Loader=yaml.FullLoader)
+with open('/home/runner/work/TFB-Network/TFB-Network/.github/workflows/scripts/live_values.yml') as fp:
+    live_values = yaml.load(fp)
 
 print("live values: ", live_values)
 output = {}
@@ -15,8 +15,8 @@ name = 'getvalues_result'
 
 for file in file_path:
     if file in live_values:
-        with open('/home/runner/work/TFB-Network/TFB-Network/' + file, 'r') as f:
-            yamlFileFromLive = yaml.unsafe_load(f)
+        with open('/home/runner/work/TFB-Network/TFB-Network/') as fp:
+            yamlFileFromLive = yaml.round_trip_load(fp)
         result[live_values[file]] = yamlFileFromLive[live_values[file]]
         output[file] = result
         result = {}
