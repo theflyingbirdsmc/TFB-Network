bec409a3bba5c7be8ac32d37110fd8d7c74c1414
Simon Fugl <sifug20@student.sdu.dk>
2022-12-05 22:34:51 +0100

    Just saving

--- .github/workflows/transform.py
+++ .github/workflows/transform.py
@@ -5,20 +5,19 @@ yaml = ruamel.yaml.YAML()
 yaml.preserve_quotes = True
 
 live_values = os.environ.get('LIVE_VALUES')
-print(type(live_values))
-print(live_values)
-# with open('/home/runner/work/TFB-Network/TFB-Network/.github/workflows/scripts/live_values.yml') as f:
-#     live_values = yaml.load(f, Loader=yaml.FullLoader)
+print("live values: ", live_values)
 
-# print("live values: ", live_values)
-# output = {}
-# name = 'getvalues_result'
+with open('/home/runner/work/TFB-Network/TFB-Network/.github/workflows/scripts/live_values.yml') as f:
+    live_values = yaml.load(f, Loader=yaml.FullLoader)
 
-# for key, value in live_values:
-#     if key.startswith("TFB-"):
-#         with open('/home/runner/work/TFB-Network/TFB-Network/' + key, 'r') as f:
-#             yamlFileFromDev = yaml.load(f)
-#         if 
+output = {}
 
-# with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-#     print(f'{name}={output}', file=fh)
+for key, value in live_values:
+    if key.startswith("TFB-"):
+        with open('/home/runner/work/TFB-Network/TFB-Network/' + key, 'r') as f:
+            yamlFileFromDev = yaml.load(f)
+        if 
+
+name = 'getvalues_result'
+with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+    print(f'{name}={output}', file=fh)
