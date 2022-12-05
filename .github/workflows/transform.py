5a00842063158365ae8943f1f08b96a6ce21c2fa
Simon Fugl <sifug20@student.sdu.dk>
2022-12-05 21:05:41 +0100

    Convert to just print

--- .github/workflows/transform.py
+++ .github/workflows/transform.py
@@ -1,6 +1,5 @@
 import os
 import ruamel.yaml
-import json
 
 yaml = ruamel.yaml.YAML()
 yaml.preserve_quotes = True
@@ -8,9 +7,6 @@ yaml.preserve_quotes = True
 live_values = os.environ.get('LIVE_VALUES')
 print(type(live_values))
 print(live_values)
-
-res = json.loads(live_values)
-print(str(res))
 # with open('/home/runner/work/TFB-Network/TFB-Network/.github/workflows/scripts/live_values.yml') as f:
 #     live_values = yaml.load(f, Loader=yaml.FullLoader)
 
