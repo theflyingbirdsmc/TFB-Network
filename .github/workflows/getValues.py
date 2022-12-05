1d44f067ba4069bbef9e65ff8d722e9b8053253f
Simon Fugl <sifug20@student.sdu.dk>
2022-12-05 21:48:53 +0100

    lifesucks

--- .github/workflows/getValues.py
+++ .github/workflows/getValues.py
@@ -17,10 +17,11 @@ for file in file_path:
     if file in live_values:
         with open('/home/runner/work/TFB-Network/TFB-Network/' + file) as fp:
             yamlFileFromLive = ruamel.yaml.round_trip_load(fp)
+            print(type(yamlFileFromLive))
         result[live_values[file]] = yamlFileFromLive[live_values[file]]
         output[file] = result
         result = {}
 
-print("OUTPUT: " + str(output))
+finalResult = ruamel.yaml.round_trip_dump(output)
 with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-    print(f'{name}={str(output)}', file=fh)
+    print(f'{name}={output}', file=fh)
