9b9b8e43358ad69098e36d295da292475f352c98
Simon Fugl <sifug20@student.sdu.dk>
2022-12-12 23:40:22 +0100

    Booleans are strings dammit

--- .github/workflows/getValues.py
+++ .github/workflows/getValues.py
@@ -7,15 +7,15 @@ file_path = os.environ['FILE_PATHS'].split(" ")
 print(file_path)
 
 # remove resolver entries for True/False
-for ch in "TtFf":
-    if len(Resolver.yaml_implicit_resolvers[ch]) == 1:
-        del Resolver.yaml_implicit_resolvers[ch]
-    else:
-        Resolver.yaml_implicit_resolvers[ch] = [x for x in
-                Resolver.yaml_implicit_resolvers[ch] if x[0] != 'tag:yaml.org,2002:bool']
+# for ch in "TtFf":
+#     if len(Resolver.yaml_implicit_resolvers[ch]) == 1:
+#         del Resolver.yaml_implicit_resolvers[ch]
+#     else:
+#         Resolver.yaml_implicit_resolvers[ch] = [x for x in
+#                 Resolver.yaml_implicit_resolvers[ch] if x[0] != 'tag:yaml.org,2002:bool']
 
 with open('/home/runner/work/TFB-Network/TFB-Network/.github/workflows/scripts/live_values.yml') as f:
-    live_values = yaml.load(f, Loader=yaml.FullLoader)
+    live_values = yaml.load(f, Loader=yaml.BaseLoader)
 
 # print("live values: ", live_values)
 output = {}
@@ -25,7 +25,7 @@ name = 'getvalues_result'
 for file in file_path:
     if file in live_values:
         with open('/home/runner/work/TFB-Network/TFB-Network/' + file, 'r') as f:
-            yamlFileFromLive = yaml.load(f, Loader=yaml.FullLoader)
+            yamlFileFromLive = yaml.load(f, Loader=yaml.BaseLoader)
             print(yamlFileFromLive)
         result[live_values[file]] = yamlFileFromLive[live_values[file]]
         output[file] = result
