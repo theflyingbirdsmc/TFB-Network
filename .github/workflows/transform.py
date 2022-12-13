cbc5a49848f4e4b101ffd00dac1cc22e56d0ca3b
Simon Fugl <sifug20@student.sdu.dk>
2022-12-13 10:12:23 +0100

    Going back for real to what worked

--- .github/workflows/transform.py
+++ .github/workflows/transform.py
@@ -1,13 +1,16 @@
 import os
-from ruamel.yaml import YAML
+import ruamel.yaml
 
-yaml = YAML()
+yaml = ruamel.yaml.YAML()
+yaml.preserve_quotes = True
+
 live_values = os.environ.get('LIVE_VALUES')
 print("live values: ", live_values)
 
-# with open('/home/runner/work/TFB-Network/TFB-Network/.github/workflows/scripts/live_values.yml') as f:
-#     live_values = yaml.load(f)
+with open('/home/runner/work/TFB-Network/TFB-Network/.github/workflows/scripts/live_values.yml') as f:
+    live_values = yaml.load(f)
 
+output = {}
 
 for value in live_values:
     if value.startswith("TFB-"):
@@ -15,43 +18,10 @@ for value in live_values:
             yamlFileFromDev = yaml.load(f)
         for elem in yamlFileFromDev:
             print(str(elem), str(live_values[value]))
-            if elem == live_values[value]:
+            if elem == value:
                 print("FOUND IT!")
                 # yaml.dump(yamlFileFromDev)
                 break
-
-# name = 'getvalues_result'
-# with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-#     print(f'{name}={yamlFileFromDev}', file=fh)
-
-
-# Save the updated dictionary to the YAML file
-# with open("data.yaml", "w") as file:
-#   yaml.dump(data, file)
-
-
-
-# import ruamel.yaml
-
-# yaml = ruamel.yaml.YAML()
-# yaml.preserve_quotes = True
-
-# live_values = os.environ.get('LIVE_VALUES')
-# print("live values: ", live_values)
-
-# with open('/home/runner/work/TFB-Network/TFB-Network/.github/workflows/scripts/live_values.yml') as f:
-#     live_values = yaml.load(f)
-
-# output = {}
-
-# for value in live_values:
-#     if value.startswith("TFB-"):
-#         with open('/home/runner/work/TFB-Network/TFB-Network/' + value) as f:
-#             yamlFileFromDev = yaml.load(f)
-#         for elem in yamlFileFromDev:
-#             print(str(elem), str(live_values[value]))
-#             if elem == value:
-#                 print("FOUND IT!")
-#                 # yaml.dump(yamlFileFromDev)
-#                 break
-
+name = 'getvalues_result'
+with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+    print(f'{name}={output}', file=fh)
