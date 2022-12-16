0e202b472451791da7ca80b0e72d436dacf5f081
Simon Fugl <sifug20@student.sdu.dk>
2022-12-16 12:04:08 +0100

    This is working locally

--- .github/workflows/getValues.py
+++ .github/workflows/getValues.py
@@ -4,8 +4,12 @@ from collections import OrderedDict
 
 yaml = ruamel.yaml.YAML()
 yaml.preserve_quotes = False
-file_path = os.environ['FILE_PATHS'].split(" ")
+# file_path = os.environ['FILE_PATHS'].split(" ")
+file_path = ['.github/workflows/getValues.py', '.github/workflows/live.yml', '.github/workflows/transform.py', 'TFB-Creative/plugins/Plan/config.yml', 'TFB-Flamecord/config.yml']
 print(file_path)
+
+# root_path = '/home/runner/work/TFB-Network/TFB-Network/.github/workflows/scripts/live_values.yml'
+root_path = 'E:/The Flying Birds/TFB-Dev/TFB-Network/'
 
 # remove resolver entries for True/False
 # for ch in "TtFf":
@@ -14,7 +18,7 @@ print(file_path)
 #     else:
 #         Resolver.yaml_implicit_resolvers[ch] = [x for x in
 #                 Resolver.yaml_implicit_resolvers[ch] if x[0] != 'tag:yaml.org,2002:bool']
-with open('/home/runner/work/TFB-Network/TFB-Network/.github/workflows/scripts/live_values.yml', "r") as file:
+with open(root_path + '.github/workflows/scripts/live_values.yml', "r") as file:
     live_values = yaml.load(file)
 
 # print("live values: ", live_values)
@@ -23,12 +27,14 @@ result = OrderedDict()
 name = 'getvalues_result'
 
 for file in file_path:
+    print('FILE PATH: ' + file)
     if file in live_values:
-        with open('/home/runner/work/TFB-Network/TFB-Network/' + file, 'r') as f:
+        with open(root_path + file, 'r') as f:
             yamlFileFromLive = yaml.load(f)
         result[live_values[file]] = yamlFileFromLive[live_values[file]]
         output[file] = result
         result= {}
 print(output)
-with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-    print(f'{name}={output}', file=fh)
+
+# with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+#     print(f'{name}={output}', file=fh)
