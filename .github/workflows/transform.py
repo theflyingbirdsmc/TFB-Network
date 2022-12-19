617c6cb37b8ee28366b90991f601003892132e09
Simon Fugl <sifug20@student.sdu.dk>
2022-12-19 20:04:49 +0100

    Change to ci

--- .github/workflows/transform.py
+++ .github/workflows/transform.py
@@ -4,13 +4,14 @@ import ast
 import json
 
 yaml = ruamel.yaml.YAML()
-yaml.preserve_quotes = True
+yaml.preserve_quotes = False
 
 root_path = "/home/runner/work/TFB-Network/TFB-Network/"
 # root_path = "C:/Users/simon/Documents/Projects/TFB-Network/"
-input_data = ast.literal_eval(os.environ.get('LIVE_VALUES'))
+# input_data = ast.literal_eval(os.environ.get('LIVE_VALUES'))
+# input_data = {"TFB-Creative/plugins/Plan/config.yml": {"Database": {"Type": "SQLite", "MySQL": {"Host": "localhost", "Port": "3306", "User": "root", "Password": "minecraft", "Database": "Plan", "Launch_options": "?rewriteBatchedStatements=true&useSSL=false&serverTimezone=UTC", "Max_connections": "8"}}}, "TFB-Flamecord/config.yml": {"servers": {"crowdcontrol": {"address": "3333333333.18.0.1:25606", "motd": "", "restricted": "false"}, "factions": {"address": "172.18.0.1:25603", "motd": "", "restricted": "false"}, "kitpvp": {"address": "172.18.0.1:25607", "motd": "", "restricted": "false"}, "lobby": {"address": "172.18.0.1:25600", "motd": "", "restricted": "false"}, "museum": {"address": "172.18.0.1:25605", "motd": "", "restricted": "false"}, "parkour": {"address": "172.18.0.1:25604", "motd": "", "restricted": "false"}, "vanilladk": {"address": "172.18.0.1:25700", "motd": "", "restricted": "false"}}}}
 # input_data = {'TFB-Creative/plugins/Plan/config.yml': {'Database': {'Type': 'SQLite', 'MySQL': {'Host': 'localhost', 'Port': '3306', 'User': 'root', 'Password': 'minecraft', 'Database': 'Plan', 'Launch_options': '?rewriteBatchedStatements=true&useSSL=false&serverTimezone=UTC', 'Max_connections': '8'}}}, 'TFB-Flamecord/config.yml': {'servers': {'crowdcontrol': {'address': '3333333333.18.0.1:25606', 'motd': '', 'restricted': 'false'}, 'factions': {'address': '172.18.0.1:25603', 'motd': '', 'restricted': 'false'}, 'kitpvp': {'address': '172.18.0.1:25607', 'motd': '', 'restricted': 'false'}, 'lobby': {'address': '172.18.0.1:25600', 'motd': '', 'restricted': 'false'}, 'museum': {'address': '172.18.0.1:25605', 'motd': '', 'restricted': 'false'}, 'parkour': {'address': '172.18.0.1:25604', 'motd': '', 'restricted': 'false'}, 'vanilladk': {'address': '172.18.0.1:25700', 'motd': '', 'restricted': 'false'}}}}
-print("live values: ", type(input_data))
+# print("live values: ", live_values)
 
 # with open('/home/runner/work/TFB-Network/TFB-Network/.github/workflows/scripts/live_values.yml') as f:
 #     live_values = yaml.load(f)
@@ -37,6 +38,6 @@ for file_path, file_data in input_data.items():
     with open(root_path + file_path, "w") as file:
         yaml.dump(config_data, file)
 
-# name = 'getvalues_result'
-# with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-#     print(f'{name}={output}', file=fh)
+name = 'transformed_files'
+with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+    print(f'{name}={output}', file=fh)
