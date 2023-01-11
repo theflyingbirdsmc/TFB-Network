da7fca5ee0af74777b2e87fd8bc20c64fedee244
Simon Fugl <sifug20@student.sdu.dk>
2023-01-11 10:46:12 +0100

    I hope we can deploy now

--- .github/workflows/deploy.py
+++ .github/workflows/deploy.py
@@ -17,8 +17,6 @@ for file in file_path:
     for server, username in server_sftpusernames.items():
         if server in file:
             print("I'm + " + server_sftpusernames[server] + "for the server " + server)
-    # with pysftp.Connection('mc.theflyingbirds.net', username='', password='secret') as sftp:
-
-    #     with sftp.cd('/allcode'):           # temporarily chdir to allcode
-    #         sftp.put('/pycode/filename')  	# upload file to allcode/pycode on remote
-    #         sftp.get('remote_file')         # get a remote file
+            with pysftp.Connection('germany01.theflyingbirds.net', username=server_sftpusernames[server], password=SFTP_TFB_PASSWORD, port=2022) as sftp:
+                with sftp.cd('/home/container'):
+                    sftp.put(file)  	# upload file to /home/container on remote
