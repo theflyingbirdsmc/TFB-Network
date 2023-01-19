b0762a10239db7d36b8b249465eeab5bddb3a000
Simon Fugl <sifug20@student.sdu.dk>
2023-01-19 16:43:48 +0100

    I just want to work with artifactory

--- .github/workflows/scripts/transform.sh
+++
@@ -1,5 +0,0 @@
-yaml_file="/home/runner/work/TFB-Network/TFB-Network/TFB-Flamecord/config.yml"
-key="disabled_commands"
-new_value="'- disabledcommandhere'"
-
-sed -r "s/^(\s*${key}\s*:\s*).*/\1${new_value}/" -i "$yaml_file"
