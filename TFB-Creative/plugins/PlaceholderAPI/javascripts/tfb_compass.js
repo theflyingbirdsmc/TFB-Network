9273948fff57ed87c1ed96c469ba460abbf98cce
Fuglen <fuglenbusiness@gmail.com>
2023-01-28 21:39:05 +0000

    Updating whole /tfb with new hex colors

--- TFB-Creative/plugins/PlaceholderAPI/javascripts/tfb_compass.js
+++ TFB-Creative/plugins/PlaceholderAPI/javascripts/tfb_compass.js
@@ -1,29 +1,29 @@
 var lang = "";
 var type = "";
 function tfbCompass() {
-   if (args.length == 2) {
-       type = args[0];
-       lang = args[1];
-   }
-    
-    if(type == "name") {
-        switch(lang) {
+    if (args.length == 2) {
+        type = args[0];
+        lang = args[1];
+    }
+
+    if (type == "name") {
+        switch (lang) {
             case "da":
-                return "#AA0000T#0000AAF#00AA00B &eMenu &7(&a/tfb&7)";
+                return "#ee2b23T#3551d7F#00ff00B &eMenu &7(&a/tfb&7)";
                 break;
             default:
-                return "#AA0000T#0000AAF#00AA00B &eMenu &7(&a/tfb&7)";
+                return "#ee2b23T#3551d7F#00ff00B &eMenu &7(&a/tfb&7)";
         }
-            
-    } else if (type  == "lore") {
-        switch(lang) {
+
+    } else if (type == "lore") {
+        switch (lang) {
             case "da":
                 return "&7Servere, Discord, Shop & meget mere!\\n\\n&aHøjreklik for at åbne";
                 break;
             default:
                 return "&7Servers, Discord,\\n&7Shop & much more!\\n\\n&aRight click to open";
         }
-    } else {return "error";}
+    } else { return "error"; }
 }
 
 tfbCompass();
--- TFB-Lobby/plugins/PlaceholderAPI/javascripts/tfb_discord.js
+++ TFB-Lobby/plugins/PlaceholderAPI/javascripts/tfb_discord.js
@@ -1,37 +1,37 @@
 var lang = "";
 var type = "";
 function tfbDiscord() {
-   if (args.length == 2) {
-       type = args[0];
-       lang = args[1];
-   }
-    
-    if(type == "name") {
-        switch(lang) {
+    if (args.length == 2) {
+        type = args[0];
+        lang = args[1];
+    }
+
+    if (type == "name") {
+        switch (lang) {
             case "da":
-                return "#AA0000T#0000AAF#00AA00B #5865F2Discord";
+                return "#ee2b23T#3551d7F#00ff00B #5865F2Discord";
                 break;
             default:
-                return "#AA0000T#0000AAF#00AA00B #5865F2Discord";
+                return "#ee2b23T#3551d7F#00ff00B #5865F2Discord";
         }
-            
-    } else if (type  == "lore") {
-        switch(lang) {
+
+    } else if (type == "lore") {
+        switch (lang) {
             case "da":
-                return "&eKlik her for et link i chatten!\\n\\n&f- &aBliv opdateret om alt med #AA0000T#0000AAF#00AA00B &a;)\\n&f- &aFå din #AA0000T#0000AAF#00AA00B &arank på vores Discord\\n&f- &aMød andre fugle du kan spille med :>";
+                return "&eKlik her for et link i chatten!\\n\\n&f- &aBliv opdateret om alt med #ee2b23T#3551d7F#00ff00B &a;)\\n&f- &aFå din #ee2b23T#3551d7F#00ff00B &arank på vores Discord\\n&f- &aMød andre fugle du kan spille med :>";
                 break;
             default:
-                return "&eClick me to get a link in chat!\\n\\n&f- &aStay connected with everything #AA0000T#0000AAF#00AA00B &a;)\\n&f- &aGet your #AA0000T#0000AAF#00AA00B &arank as a Discord role\\n&f- &aMeet other birds :>";
+                return "&eClick me to get a link in chat!\\n\\n&f- &aStay connected with everything #ee2b23T#3551d7F#00ff00B &a;)\\n&f- &aGet your #ee2b23T#3551d7F#00ff00B &arank as a Discord role\\n&f- &aMeet other birds :>";
         }
-    } else if (type  == "message") {
-        switch(lang) {
+    } else if (type == "message") {
+        switch (lang) {
             case "da":
                 return "&adiscord.theflyingbirds.net&r &8<- &eKlik her for at joine!";
                 break;
             default:
                 return "&adiscord.theflyingbirds.net&r &8<- &eClick me to join!";
         }
-    } else {return "error";}
+    } else { return "error"; }
 }
 
 tfbDiscord();
