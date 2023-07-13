737929596f35e491d85c26bb11834964b86dcb30
Simon Fugl <sifug20@student.sdu.dk>
2023-07-13 19:48:49 +0200

    Updated all database names

--- .devcontainer/init.sql
+++ .devcontainer/init.sql
@@ -1,55 +1,55 @@
 -- LUCKPERMS --
 DROP USER IF EXISTS 'tfb_luckperms';
 CREATE USER 'tfb_luckperms'@'%';
-CREATE DATABASE IF NOT EXISTS s1_luckperms;
-GRANT ALL ON s1_luckperms.* TO 'tfb_luckperms'@'%' IDENTIFIED BY 'uEJq!fjGM!62*&cmX*mBRhT9L2@SgX';
+CREATE DATABASE IF NOT EXISTS luckperms;
+GRANT ALL ON luckperms.* TO 'tfb_luckperms'@'%' IDENTIFIED BY 'uEJq!fjGM!62*&cmX*mBRhT9L2@SgX';
 
 -- PLAN --
 DROP USER IF EXISTS 'tfb_plan'; 
 CREATE USER 'tfb_plan'@'%';
-CREATE DATABASE IF NOT EXISTS s1_plan;
-GRANT ALL ON s1_plan.* TO 'tfb_plan'@'%' IDENTIFIED BY '$Z@jJKSLcd7xUekU5qF&VW3#ndU&mm';
+CREATE DATABASE IF NOT EXISTS plan;
+GRANT ALL ON plan.* TO 'tfb_plan'@'%' IDENTIFIED BY '$Z@jJKSLcd7xUekU5qF&VW3#ndU&mm';
 
 -- VOTINGPLUGIN --
 DROP USER IF EXISTS 'tfb_votingplugin';
 CREATE USER 'tfb_votingplugin'@'%';
-CREATE DATABASE IF NOT EXISTS s1_votingplugin;
-GRANT ALL ON s1_votingplugin.* TO 'tfb_votingplugin'@'%' IDENTIFIED BY '5wZw5@ySSE@zZSd4b#QFrPfsedwhzz';
+CREATE DATABASE IF NOT EXISTS votingplugin;
+GRANT ALL ON votingplugin.* TO 'tfb_votingplugin'@'%' IDENTIFIED BY '5wZw5@ySSE@zZSd4b#QFrPfsedwhzz';
 
 -- GADGETSMENU --
-DROP USER IF EXISTS 'u2_oeN5DZ9Msp';
-CREATE USER 'u2_oeN5DZ9Msp'@'%';
-CREATE DATABASE IF NOT EXISTS s2_gadgetsmenu;
-GRANT ALL ON s2_gadgetsmenu.* TO 'u2_oeN5DZ9Msp'@'%' IDENTIFIED BY '^DqHSSNgoX.IQCre6=.x2.J4';
+DROP USER IF EXISTS 'tfb_gadgetsmenu';
+CREATE USER 'tfb_gadgetsmenu'@'%';
+CREATE DATABASE IF NOT EXISTS gadgetsmenu;
+GRANT ALL ON gadgetsmenu.* TO 'tfb_gadgetsmenu'@'%' IDENTIFIED BY 'vF53CRgmZt*@8y6y3f#ku&L*d4*fj^';
 
 -- CHATCONTROLRED --
 DROP USER IF EXISTS 'tfb_chatcontrolred';
 CREATE USER 'tfb_chatcontrolred'@'%';
-CREATE DATABASE IF NOT EXISTS s2_chatcontrolred;
-GRANT ALL ON s2_chatcontrolred.* TO 'tfb_chatcontrolred'@'%' IDENTIFIED BY 'e&cqyYXtqjgfL!hEGMyFpqMi89!g9w';
+CREATE DATABASE IF NOT EXISTS chatcontrolred;
+GRANT ALL ON chatcontrolred.* TO 'tfb_chatcontrolred'@'%' IDENTIFIED BY 'e&cqyYXtqjgfL!hEGMyFpqMi89!g9w';
 
 -- PLOTSQUARED --
-DROP USER IF EXISTS 'u5_DEXeIJnxSl';
-CREATE USER 'u5_DEXeIJnxSl'@'%';
-CREATE DATABASE IF NOT EXISTS s5_plotsquared;
-GRANT ALL ON s5_plotsquared.* TO 'u5_DEXeIJnxSl'@'%' IDENTIFIED BY 'x2IQ^K4prZAW=^uTzpGMMG@u';
+DROP USER IF EXISTS 'tfb_plotsquared';
+CREATE USER 'tfb_plotsquared'@'%';
+CREATE DATABASE IF NOT EXISTS plotsquared;
+GRANT ALL ON plotsquared.* TO 'tfb_plotsquared'@'%' IDENTIFIED BY 'kBk4x@oQq8ehZ8jF*3mUn&3HMrBz@f';
 
 -- COREPROTECT --
-DROP USER IF EXISTS 'u9_OuEs5LpxqL';
-CREATE USER 'u9_OuEs5LpxqL'@'%';
-CREATE DATABASE IF NOT EXISTS s9_coreprotect;
-GRANT ALL ON s9_coreprotect.* TO 'u9_OuEs5LpxqL'@'%' IDENTIFIED BY '9l!C+mt2.4MvHb+BqnVkEU+f';
+DROP USER IF EXISTS 'tfb_coreprotect';
+CREATE USER 'tfb_coreprotect'@'%';
+CREATE DATABASE IF NOT EXISTS coreprotect;
+GRANT ALL ON coreprotect.* TO 'tfb_coreprotect'@'%' IDENTIFIED BY 'J$ca$cXuj!HQEijVEjh69KuTYC998E';
 FLUSH PRIVILEGES;
 
 -- PLAYERREFERALS --
 DROP USER IF EXISTS 'tfb_playerreferrals';
 CREATE USER 'tfb_playerreferrals'@'%';
-CREATE DATABASE IF NOT EXISTS s2_playerreferals;
-GRANT ALL ON s2_playerreferals.* TO 'tfb_playerreferrals'@'%' IDENTIFIED BY 'bxmJaWd#264#aT6JW$UQaQAmq3hbYd';
+CREATE DATABASE IF NOT EXISTS playerreferals;
+GRANT ALL ON playerreferals.* TO 'tfb_playerreferrals'@'%' IDENTIFIED BY 'bxmJaWd#264#aT6JW$UQaQAmq3hbYd';
 FLUSH PRIVILEGES;
 
 -- HEXNICKS --
 DROP USER IF EXISTS 'tfb_hexnicks';
 CREATE USER 'tfb_hexnicks'@'%';
-CREATE DATABASE IF NOT EXISTS s2_hexnicks;
-GRANT ALL ON s2_hexnicks.* TO 'tfb_hexnicks'@'%' IDENTIFIED BY 'j77RqV*scJQ2RSjcx@nRNiXNU&Yow*';
+CREATE DATABASE IF NOT EXISTS hexnicks;
+GRANT ALL ON hexnicks.* TO 'tfb_hexnicks'@'%' IDENTIFIED BY 'j77RqV*scJQ2RSjcx@nRNiXNU&Yow*';
