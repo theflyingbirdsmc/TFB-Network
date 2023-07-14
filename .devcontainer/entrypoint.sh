668975f98c993b514a853e96d185c40c8e8b5684
Simon Fugl <sifug20@student.sdu.dk>
2023-07-14 16:33:12 +0200

    Add TFB-Dev Docker image

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -82,8 +82,13 @@
 #     echo -e "Running curl -o ${SERVER_JARFILE} ${DOWNLOAD_URL}"
 #     curl -o ${SERVER_JARFILE} ${DOWNLOAD_URL}
 
-cd /home/container/TFB
-find /home/container/TFB/ -type f -iname "*.sh" -exec chmod +x {} \; # Make all .sh files executeable
+# cd /home/container/TFB
+# find /home/container/TFB/ -type f -iname "*.sh" -exec chmod +x {} \; # Make all .sh files executeable
 #tmux new -s mysession -n console && tmux ls
 #java -Xms128M -Xmx1024M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -Dterminal.jline=false -Dterminal.ansi=true -jar server.jar nogui
 #exec tail -f /dev/null
+
+if [ ! -d "TFB" ]
+then
+    git clone https://github.com/theflyingbirdsmc/TFB-Network TFB
+fi
