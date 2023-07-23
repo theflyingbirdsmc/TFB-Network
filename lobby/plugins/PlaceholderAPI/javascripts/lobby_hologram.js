76eed9c4ff5565b45025e2a6512f5d9c7dd9b88b
Fuglen <fuglenbusiness@gmail.com>
2023-07-23 20:23:33 +0000

    Translated to Danish and added bird emotes

--- lobby/plugins/PlaceholderAPI/javascripts/lobby_hologram.js
+++ lobby/plugins/PlaceholderAPI/javascripts/lobby_hologram.js
@@ -9,16 +9,16 @@ function translate() {
     if (type == "1") {
         switch (lang) {
             case "da":
-                return "&eVelkommen";
+                return "&f🕊 &eVelkommen til &#ee2b23T&#f04923h&#f26622e &#3551d7F&#366dd9l&#3789dby&#38a4ddi&#39c0dfn&#3adce1g &#00ff00B&#3fff00i&#7dff00r&#bcff00d&#faff00s &f🕊";
                 break;
             default:
-                return "&eWelcome to &#ee2b23T&#f04923h&#f26622e &#3551d7F&#366dd9l&#3789dby&#38a4ddi&#39c0dfn&#3adce1g &#00ff00B&#3fff00i&#7dff00r&#bcff00d&#faff00s";
+                return "&f🕊 &eWelcome to &#ee2b23T&#f04923h&#f26622e &#3551d7F&#366dd9l&#3789dby&#38a4ddi&#39c0dfn&#3adce1g &#00ff00B&#3fff00i&#7dff00r&#bcff00d&#faff00s &f🕊";
         }
 
     } else if (type == "3") {
         switch (lang) {
             case "da":
-                return "";
+                return "&a🧭 &aKlik her &eeller brug kompasset for at komme i gang &a🧭";
                 break;
             default:
                 return "&a🧭 &aClick here &eor use the &acompass &eto get started &a🧭";
