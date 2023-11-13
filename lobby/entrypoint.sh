154c1ab257966d3bf79a41adab019595d49f5135
Fuglen <fuglenbusiness@gmail.com>
2023-11-13 21:42:47 +0000

    oupsie

--- lobby/entrypoint.sh
+++ lobby/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Create the symlink after the command finishes
-ln -s /external-folder/user.yml /tfb/plugins/StreamingDrops/user.yml
+ln -sf /tfb/plugins/StreamingDrops/user.yml /tfb/plugins/StreamingDrops/data/user.yml
 
 # Run the original command (Minecraft server in this case)
 java -Xms128M -Xmx4096M -jar server.jar nogui
