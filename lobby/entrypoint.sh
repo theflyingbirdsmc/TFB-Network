8cf85d261351526735f4ffb4ef0dab8d11768915
Fuglen <fuglenbusiness@gmail.com>
2023-11-13 22:10:42 +0000

    Trying hardlink instead

--- lobby/entrypoint.sh
+++ lobby/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-
+cp /tfb/plugins/StreamingDrops/data/user.yml /tfb/plugins/StreamingDrops/user.yml
 # Create the symlink after the command finishes
-ln -sf /tfb/plugins/StreamingDrops/user.yml /tfb/plugins/StreamingDrops/data/user.yml
+ln -hf /tfb/plugins/StreamingDrops/user.yml /tfb/plugins/StreamingDrops/data/user.yml
 
 # Run the original command (Minecraft server in this case)
 java -Xms128M -Xmx4096M -jar server.jar nogui
